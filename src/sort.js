6a19b84d4b7685a1658c9d94edc6c8cdb2217232
VladDadonkin <vlad.dadonkin.02@mail.ru>
2024-11-12 00:08:19 +0300

    add test

--- src/sort.js
+++ src/sort.js
@@ -1,21 +1,24 @@
 
-const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
-const order = ["name", "level"];
-
-function orderByProps(obj, order) {
+export function orderByProps(obj, order) {
     const result = [];
-
-    for (let key in order) {
-        if (key in obj) {
-            result.push({ key, value: obj[key] });
-            
-        };
-    };
-
-    const sort = Object.keys(obj).filter(key => !order.includes(key)).sort();
-    
+  
+    for (const key of order) {
+      if (key in obj) {
+        result.push({ key, value: obj[key] });
+      }
+    }
+  
+    const sortedKeys = Object.keys(obj)
+      .filter(key => !order.includes(key))
+      .sort();
+  
+    for (const key of sortedKeys) {
+      result.push({ key, value: obj[key] });
+    }
+  
     return result;
-};
+  }
+  
 
 
 
